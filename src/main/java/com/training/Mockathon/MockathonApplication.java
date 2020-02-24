5d84a1c01b9d03c94280e4ebc0080077481abc79
Vijayan Raja shanmugakani <vijayan.r@hcl.com>
2020-02-24 19:52:46 +0530

    First Check-in

---
+++ src/main/java/com/training/Mockathon/MockathonApplication.java
@@ -0,0 +1,13 @@
+package com.training.Mockathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/Mockathon/MockathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.Mockathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
